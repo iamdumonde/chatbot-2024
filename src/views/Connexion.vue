77ca6ed4d60403c668c090b69752b6f92bb6c7bf
Delanno Kotcho <delanktc24@gmail.com>
2024-01-09 16:29:34 +0100

    installation de icon

--- src/views/Connexion.vue
+++ src/views/Connexion.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex items-center justify-center h-screen bg-gray-100">
         <div class=" w-1/3 bg-white p-8 rounded-md shadow-md">
-            <div class="flex justify-center">                
-                    <!-- <img src="../assets/logo.png" alt="" class="w-20"/> -->
-                    <p> <img src="../assets/logo.png" alt="" class="w-20"/> Inscription</p>
+            <div class="flex justify-center">
+                <!-- <img src="../assets/logo.png" alt="" class="w-20"/> -->
+                <p> <img src="../assets/logo.png" alt="" class="w-20" /> Inscription</p>
             </div>
             <form @submit.prevent="submit" class="mt-6 mb-7">
                 <div>
@@ -13,22 +13,25 @@
                     <input id="name" type="text" class="mt-3 py-3 p-2 border  border-gray-300 rounded-md block w-full"
                         v-model="form.userName" required autofocus autocomplete="name" />
                 </div>
-                
+
                 <div class="mt-4">
                     <label for="password" class="block text-xl font-medium text-gray-700">
                         Password
                     </label>
-                    <input id="password" type="password" class="mt-3 py-3 p-2 border  border-gray-300 rounded-md block w-full"
-                        v-model="form.password" required autocomplete="new-password" />
+                    <input id="password" type="password"
+                        class="mt-3 py-3 p-2 border  border-gray-300 rounded-md block w-full" v-model="form.password"
+                        required autocomplete="new-password" />
                 </div>
 
                 <div class="flex justify-between mt-6">
                     <div class="text-xl text-gray-600 hover:text-gray-900">
-                        <router-link :to="{name: 'inscription'}">
-                            J'ai un compte
-                        </router-link >
+                        <button @click="handleLoginClick" :disabled="isLoading"
+                            class="underline cursor-pointer focus:outline-none">
+                            {{ isLoading ? 'Chargement...' : 'J\'ai pas de compte' }}
+                           
+                        </button>
                     </div>
-                    <button type="submit"
+                    <button type="submit" :disabled="isLoading"
                         class="ml-4 px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:border-indigo-500">
                         Se connecter
                     </button>
@@ -38,7 +41,11 @@
     </div>
 </template>
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const isLoading = ref(false);
+const router = useRouter();
 
 const form = reactive({
     userName: '',
@@ -47,5 +54,19 @@ const form = reactive({
 
 const submit = () => {
     //  envoie les données dans la base de donnée pour faire la vérification
+};
+
+const handleLoginClick = async () => {
+    try {
+        isLoading.value = true;
+
+        // Simulez un chargement pendant 1.5 secondes 
+        await new Promise(resolve => setTimeout(resolve, 1500));
+
+        // Redirection vers la page de connexion
+        router.push({ name: 'inscription' });
+    } finally {
+        isLoading.value = false;
+    }
 };
 </script>
--- src/views/UserProfil.vue
+++ src/views/UserProfil.vue
@@ -2,31 +2,78 @@
 import AuthenticatedLayoutAdmin from '@/Layouts/AuthenticatedLayoutAdmin.vue';
 <template>
     <AuthenticatedLayoutAdmin>
-        <div>
-            <p>Profil</p>
-            <section>
-                <div>
-                    <p>Avatar</p>
-                    <img src="" alt=""/>
-                </div>
-                <div>
-                    <h1>Profil Information</h1>
-                    <p>Mettez à jour les informations de profil et l'adresse e-mail de votre compte</p>
-                    <form action="">
-                        <div>
-                            <label>Username :</label>
-                            <input/>
-                        </div>
-                        <div>
-                            <label>Username :</label>
-                            <input/>
-                        </div>
-                        <button type="submit">Sauvegader</button>
-                    </form>
-                </div>
-
-                <div>
-
+        <div class="max-w-5xl mx-auto p-6">
+            <p class="text-2xl font-semibold mb-4">Profil</p>
+            <section class="flex justify-center">
+                <div class="bg-gray-200 p-6 rounded-md shadow-md w-full">
+                    <div class="flex items-center justify-center mb-6">
+                        <img src="../assets/logo.png" class="w-40" alt="User Avatar" />
+                    </div>
+                    <div class="bg-white p-8 rounded-xl">
+                        <h1 class="text-2xl font-semibold mb-2">Profil Information</h1>
+                        <p class="text-gray-600 mb-4">
+                            Mettez à jour les informations de profil et l'adresse e-mail de votre compte
+                        </p>
+                        <form @submit.prevent="submit" class="space-y-4">
+                            <div class="flex items-center space-x-3">
+                                <label for="name" class="text-xl w-32 font-medium text-gray-700">
+                                    Username :
+                                </label>
+                                <input id="name" type="text" class="mt-3 py-3 px-4 border border-gray-300 rounded-md w-full"
+                                    v-model="form.userName" required autofocus autocomplete="name" />
+                            </div>
+                            <div class="flex items-center space-x-3">
+                                <label for="email" class="text-xl w-32 font-medium text-gray-700">
+                                    Email :
+                                </label>
+                                <input id="email" type="email"
+                                    class="mt-3 py-3 px-4 border border-gray-300 rounded-md w-full" v-model="form.email"
+                                    required autocomplete="username" />
+                            </div>
+                            <div class="text-right">
+                                <button type="submit"
+                                    class="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:border-indigo-500">
+                                    Sauvegader
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="bg-white p-8 mt-4 rounded-2xl">
+                        <h1 class="text-2xl font-semibold mb-2">Mettre à jour le mot de passe</h1>
+                        <p class="text-gray-600 mb-4">
+                            Assurez-vous que votre compte utilise un mot de passe long et aléatoire pour rester en sécurité                        </p>
+                        <form @submit.prevent="submit" class="space-y-4">
+                            <div class="flex items-center space-x-3">
+                                <label for="name" class="text-xl w-32 font-medium text-gray-700">
+                                    Current Password :
+                                </label>
+                                <input id="name" type="text" class="mt-3 py-3 px-4 border border-gray-300 rounded-md w-full"
+                                    v-model="form.userName" required autofocus autocomplete="name" />
+                            </div>
+                            <div class="flex items-center space-x-3">
+                                <label for="email" class="text-xl w-32 font-medium text-gray-700">
+                                    New Password :
+                                </label>
+                                <input id="email" type="email"
+                                    class="mt-3 py-3 px-4 border border-gray-300 rounded-md w-full" v-model="form.email"
+                                    required autocomplete="username" />
+                            </div>
+                            <div class="flex items-center space-x-3">
+                                <label for="email" class="text-xl w-32 font-medium text-gray-700">
+                                    Confirm Password :
+                                </label>
+                                <input id="email" type="email"
+                                    class="mt-3 py-3 px-4 border border-gray-300 rounded-md w-full" v-model="form.email"
+                                    required autocomplete="username" />
+                            </div>
+                            <div class="text-right">
+                                <button type="submit"
+                                    class="px-4 py-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring focus:border-indigo-500">
+                                    Sauvegader
+                                </button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </section>
         </div>
@@ -34,5 +81,16 @@ import AuthenticatedLayoutAdmin from '@/Layouts/AuthenticatedLayoutAdmin.vue';
 </template>
 
 <script setup>
-    import AuthenticatedLayoutAdmin from '@/Layouts/AuthenticatedLayoutAdmin.vue';
+import AuthenticatedLayoutAdmin from '@/Layouts/AuthenticatedLayoutAdmin.vue';
+
+import { ref } from 'vue';
+
+const form = ref({
+    userName: '',
+    email: '',
+});
+
+const submit = () => {
+
+};
 </script>
